d2b5449ef68c0a6680f5b8e9e97030c7a99cd483
Sebastian Höffner <info@sebastian-hoeffner.de>
2020-08-11 15:27:32 +0200

    Updating RosSharp.

--- UnityNao/Assets/RosSharp/Scripts/Extensions/HeaderExtensions.cs
+++ UnityNao/Assets/RosSharp/Scripts/Extensions/HeaderExtensions.cs
@@ -1,13 +1,11 @@
 ﻿/*
-© Siemens AG, 2018
-Author: Suzannah Smith (suzannah.smith@siemens.com)
+© Siemens AG, 2017-2019
+Author: Berkay Alp Cakal (berkay_alp.cakal.ct@siemens.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 <http://www.apache.org/licenses/LICENSE-2.0>.
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +13,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using UnityEngine;
+// Added allocation free alternatives
+// UoK , 2019, Odysseas Doumas (od79@kent.ac.uk / odydoum@gmail.com) 
 
-namespace RosSharp.Urdf
+namespace RosSharp.RosBridgeClient
 {
-    [SelectionBase]
-    public class UrdfVisual : MonoBehaviour
+    public static class HeaderExtensions
     {
-        public GeometryTypes GeometryType;
+        private static Timer timer = new Timer();
+
+        public static void Update(this MessageTypes.Std.Header header)
+        {
+            header.seq++;
+            timer.Now(header.stamp);
+        }
     }
 }
